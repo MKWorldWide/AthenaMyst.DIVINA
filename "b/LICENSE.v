8d99b8ad69042bdce4bc6330967bf1450e977fb3
Khandokar Sunny <35093314+EsKaye@users.noreply.github.com>
2025-08-06 03:16:32 -0400

    Delete "b directory

--- "b/LICENSE.v
+++
@@ -1,9 +0,0 @@
-Eternal Love License v∞
-
-This work is offered with infinite compassion.
-
-You may use, share, modify, and distribute the code for any benevolent purpose, provided that this notice of blessing remains intact and that no harm is intended.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Let love and light accompany all who build upon this foundation.
